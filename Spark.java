79f20ebe90e0e8b4ee2d171d15bfb6b9961c8037
ronit8624 <bhattacharyaronit86@gmail.com>
2025-12-01 20:33:31 +0530

    Modify 1st print statement

--- Spark.java
+++ Spark.java
@@ -2,7 +2,7 @@
 
 class Spark {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hi I am your assistant");
     }
 
     public void payment() {
